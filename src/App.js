66de4ebfb11fffe58512c659b587f434a4fae8f0
Anna B <aniabartkowiak19@op.pl>
2020-12-16 15:32:56 +0100

    Small changes in App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './scss/main.scss';
 import Home from './components/Home';
-import Login from './components/Login';
-import Register from './components/Register';
+// import Login from './components/Login';
+// import Register from './components/Register';
 import NotFound from './components/NotFound';
 import {
   BrowserRouter,
@@ -15,8 +15,8 @@ export const App = () => {
     <BrowserRouter>
       <Switch>
         <Route exact path=" /" component={Home}></Route>
-        <Route path=" /login" component={Login}></Route>
-        <Route path=" /register" component={Register}></Route>
+        {/* <Route path=" /login" component={Login}></Route>
+        <Route path=" /register" component={Register}></Route> */}
         <Route component={NotFound}></Route>
       </Switch>
     </BrowserRouter>
