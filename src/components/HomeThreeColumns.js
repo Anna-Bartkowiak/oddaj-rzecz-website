28eb3687e58485c67642b7f1bab128431f4e3868
Anna B <aniabartkowiak19@op.pl>
2021-01-05 10:44:55 +0100

    Creating section with information about achieved goals - three columns.

--- src/components/HomeThreeColumns.js
+++ src/components/HomeThreeColumns.js
@@ -5,7 +5,23 @@ export const HomeThreeColumns = () => {
     return (
         <>
             <section className='threeColumns' style={{ 'backgroundImage': `url(${background})` }}>
-                <div className='threeColumns__container'></div>
+                <div className='threeColumns__container'>
+                    <div className='threeColumns__bags'>
+                        <p className='threeColumns__numbers'>10</p>
+                        <h2 className='threeColumns__h2'>oddanych worków</h2>
+                        <p className='threeColumns__text'>Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma. Aliquam erat volutpat.</p>
+                    </div>
+                    <div className='threeColumns__help'>
+                        <p className='threeColumns__numbers'>5</p>
+                        <h2 className='threeColumns__h2'>wspartych organizacji</h2>
+                        <p className='threeColumns__text'>Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma. Aliquam erat volutpat.</p>
+                    </div>
+                    <div className='threeColumns__collection'>
+                        <p className='threeColumns__numbers'>7</p>
+                        <h2 className='threeColumns__h2'>zorganizowanych zbiórek</h2>
+                        <p className='threeColumns__text'>Lorem ipsum dolor sit amet, consectetur adipisc Pellentesque vel enim a elit viverra elementuma. Aliquam erat volutpat.</p>
+                    </div>
+                </div>
             </section>
         </>
     )
