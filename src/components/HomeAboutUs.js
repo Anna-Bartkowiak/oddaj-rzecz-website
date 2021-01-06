b1fb40e9a3e9e2e9abc2e047e66a0a0d96856fcf
Anna B <aniabartkowiak19@op.pl>
2021-01-06 12:40:56 +0100

    Creating about us section

--- src/components/HomeAboutUs.js
+++ src/components/HomeAboutUs.js
@@ -1,10 +1,26 @@
-import React from 'react'
+import React from 'react';
+import decoration from '../assets/Decoration.svg';
+import signature from '../assets/Signature.svg';
+import team from '../assets/People@2x.png';
 
 export const HomeAboutUs = () => {
     return (
-        <div id='aboutUs'>
+        <>
+            <section id='aboutUs' className='aboutUs'>
+                <div className="aboutUs__container">
+                    <div className="aboutUs__wrapperText">
+                        <h2 className="aboutUs__h2">O nas</h2>
+                        <img src={decoration} className='aboutUs__decorationImage' alt="decoration" />
+                        <p className="aboutUs__text">Nori grape silver beet broccoli kombu beet greens fava bean potato quandong celery. Bunya nuts black-eyed pea prairie turnip leek lentil turnip greens parsnip.</p>
+                        <img src={signature} className='aboutUs__signatureImage aboutUs__signatureImage--toRight' alt="signature" />
+                    </div>
 
-        </div>
+                    <div className="aboutUs__image">
+                        <img src={team} className='aboutUs__teamImage' alt="team" />
+                    </div>
+                </div>
+            </section>
+        </>
     )
 }
 
